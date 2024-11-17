f74a9b9791915fcd4f4b3ab26f584f397d609f65
Khaled-Mohammed-Abdelgaber <66785999+Khaled-Mohammed-Abdelgaber@users.noreply.github.com>
2024-11-17 14:39:19 +0200

    Update tasks.py
    add ECABasicBlock to the if condition

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -998,7 +998,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             RepC3,
             PSA,
             SCDown,
-            C2fCIB,CBAM,
+            C2fCIB,CBAM,ECABasicBlock,
         }:
             c1, c2 = ch[f], args[0]
             if c2 != nc:  # if c2 not equal to number of classes (i.e. for Classify() output)
